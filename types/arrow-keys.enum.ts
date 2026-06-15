e3de83327deb637a6ce3b6f8066a8bcae0ed54e5
Cig4l <coralieruiz.dev@gmail.com>
2026-06-15 16:58:45 +0200

    feat(main):created  main classes files + enums + constants

---
+++ types/arrow-keys.enum.ts
@@ -0,0 +1,6 @@
+export enum ArrowKeys {
+    TOP,
+    BOTTOM,
+    RIGHT,
+    LEFT
+}
---
+++ types/falling-speed.enum.ts
@@ -0,0 +1,7 @@
+export enum FallingSpeed {
+    LEVEL1 = 1,
+    LEVEL2 = 1.5,
+    LEVEL3 = 2,
+    LEVEL4 = 2.5,
+    LEVEL5 = 3
+}
