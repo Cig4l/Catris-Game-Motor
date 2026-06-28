8ce744b104cd14e7695662d8ec65c1fd70150677
Cig4l <coralieruiz.dev@gmail.com>
2026-06-28 22:41:07 +0200

    feat(main): board methods

---
+++ src/core/board.ts
@@ -0,0 +1,35 @@
+import { Cell } from "../constants/piece-types.constants";
+import { Position } from "../models/Position";
+import { Board } from "../types/types";
+
+export function createEmptyBoard(height: number, width: number): Board {
+  return Array.from({ length: height }, () => Array(width).fill(null)) as Board;
+}
+
+export function updateActivePiecePosition(
+  board: Board,
+  positions: ReadonlyArray<Position>,
+  value: Cell,
+): Board {
+  const nextBoard: Cell[][] = board.map((row) => row.slice());
+
+  for (const position of positions) {
+    nextBoard[position.col][position.row] = value;
+  }
+
+  return nextBoard as Board;
+}
+
+export function isEmpty(position: Position, board: Board): boolean {
+  return board[position.col][position.row] == null;
+}
+
+export function isInsideBoard(position: Position, board: Board): boolean {
+  return (
+    position.col >= 0 &&
+    position.col < board.length &&
+    position.row >= 0 &&
+    position.row < board[0].length
+  );
+}
+
--- src/types/types.ts
+++ src/types/types.ts
@@ -3,4 +3,3 @@ import { Cell } from "../constants/piece-types.constants";
 export type Board = ReadonlyArray<ReadonlyArray<Cell>>;
  
 export type GameStatus = 'idle' | 'running' | 'paused' | 'gameover';
-
