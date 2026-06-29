380c1b1c651a5ff84067ed5144dd1351ad33d669
Cig4l <coralieruiz.dev@gmail.com>
2026-06-29 02:22:17 +0200

    feat(main): added methods in piece.ts

--- src/core/piece.ts
+++ src/core/piece.ts
@@ -3,6 +3,10 @@ import { PIECE_DEFAULT_POSITIONS } from "../constants/shape.constants";
 import { ActivePiece, Rotation } from "../models/ActivePiece";
 import { Position } from "../models/Position";
 
+export function createPiece(type: PieceType, pivot: Position) : ActivePiece {
+  return {type : type, pivot: pivot, rotation: 0 as Rotation, cells: calculatePositions(type, pivot, 0)};
+};
+
 export function calculatePositions(
   type: PieceType,
   pivot: Position,
@@ -14,14 +18,56 @@ export function calculatePositions(
     offsets = rotateOffsetCW(offsets);
   }
 
-  return offsets.map((o) => ({col: pivot.col + o.col, row: pivot.row + o.row}));
+  return offsets.map((o) => ({
+    col: pivot.col + o.col,
+    row: pivot.row + o.row,
+  }));
 }
 
-export function rotateOffsetCW(offsets: ReadonlyArray<Position>) : ReadonlyArray<Position> {
-    let rotateOffsets : Position[] = [];
+export function rotateOffsetCW(
+  offsets: ReadonlyArray<Position>,
+): ReadonlyArray<Position> {
+  let rotateOffsets: Position[] = [];
 
-    for(const offset of offsets) {
-        rotateOffsets.push({col: offset.row * -1, row: offset.col})
-    }
-    return rotateOffsets as ReadonlyArray<Position>;
+  for (const offset of offsets) {
+    rotateOffsets.push({ col: offset.row * -1, row: offset.col });
+  }
+  return rotateOffsets as ReadonlyArray<Position>;
+}
+
+export function move(
+  piece: ActivePiece,
+  moveX: number,
+  moveY: number,
+): ActivePiece {
+  // modifies where the active piece is in the board (moveLeft()...)
+  let pivot: Position = {
+    col: piece.pivot.col + moveX,
+    row: piece.pivot.row + moveY,
+  };
+
+  let cells: ReadonlyArray<Position> = calculatePositions(
+    piece.type,
+    pivot,
+    piece.rotation,
+  );
+
+  return { ...piece, cells: cells, pivot: pivot } as ActivePiece;
+}
+
+export function rotateCW(piece: ActivePiece): ActivePiece {
+  // modifies how active piece rotates
+  if (piece.type == "O") {
+    return piece;
+  }
+
+  let rotation: Rotation = (piece.rotation + (1 % 4)) as Rotation;
+
+  let cells: ReadonlyArray<Position> = calculatePositions(
+    piece.type,
+    piece.pivot,
+    rotation,
+  );
+
+  return { ...piece, cells: cells, rotation: rotation } as ActivePiece;
 }
